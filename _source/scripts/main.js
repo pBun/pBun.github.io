9eb0808a41d407a8013c244f63e0947b17f3d528
Dustin Boersma <dustin.boersma@gmail.com>
2017-02-20 00:32:32 -0800

    fixed blur script

--- _source/scripts/main.js
+++ _source/scripts/main.js
@@ -1,22 +1,23 @@
 'use strict';
 
 (function() {
-  var addClass = function(el, className) {
-  	if (el.classList) {
-	  el.classList.add(className);
-  	} else {
-	  el.className += ' ' + className;
-  	}
-  };
-  var waterfallElements = document.querySelectorAll('.blog .tile');
-  waterfallElements.forEach(function(el, i) {
-  	setTimeout(function () {
-  		addClass(el, 'animate');
-	}, i * 300);
-  });
-  document.querySelectorAll('a').addEventListener('click', function() {
-    var event = document.createEvent('HTMLEvents');
-	event.initEvent('blur', true, false);
-	this.dispatchEvent(event);
-  });
+    var addClass = function(el, className) {
+        if (el.classList) {
+            el.classList.add(className);
+        } else {
+            el.className += ' ' + className;
+        }
+    };
+    var waterfallElements = document.querySelectorAll('.blog .tile');
+    waterfallElements.forEach(function(el, i) {
+        setTimeout(function() {
+            addClass(el, 'animate');
+        }, i * 300);
+    });
+    var tileAnchors = document.querySelectorAll('a.tile');
+    tileAnchors.forEach(function(el, i) {
+        el.addEventListener('click', function(e) {
+            this.blur();
+        });
+    });
 })();
